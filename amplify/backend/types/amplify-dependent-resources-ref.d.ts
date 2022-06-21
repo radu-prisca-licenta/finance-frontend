6dc7eee9224571395b68eeca2052e4a539326777
Radu <radu.prisca.licenta@gmail.com>
2022-06-21 14:34:26 +0300

    fix date

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,10 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "hosting": {
-        "S3AndCloudFront": {
-            "Region": "string",
-            "HostingBucketName": "string",
-            "WebsiteURL": "string",
-            "S3BucketSecureURL": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}

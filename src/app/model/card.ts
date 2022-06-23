5a66bd190ca7337315e9c533e17db2f668cd6c18
Radu <radu.prisca.licenta@gmail.com>
2022-06-23 06:22:23 +0300

    add card service

---
+++ src/app/model/card.ts
@@ -0,0 +1,7 @@
+export interface Card {
+    cardId: string;
+    title: string;
+    account: string;
+    user: string;
+    balance: number;
+}
--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,30 +1,30 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {User} from '../model/user';
+import {Card} from '../model/card';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class CardService {
 
-  URL = 'https://ceo0djnuaa.execute-api.eu-central-1.amazonaws.com/prod/users';
+  URL = 'https://ha163hktjb.execute-api.eu-central-1.amazonaws.com/prod/cards';
 
   constructor(private http: HttpClient) { }
 
   getAll() {
-    return this.http.get<User[]>(this.URL);
+    return this.http.get<Card[]>(this.URL);
   }
 
   getById(id: string) {
     return this.http.get(this.URL + '/' + id);
   }
 
-  create(user: User) {
-    return this.http.post(this.URL, user).subscribe();
+  create(card: Card) {
+    return this.http.post(this.URL, card).subscribe();
   }
 
-  update(user: User) {
-    return this.http.put(this.URL + '/' + user.userId, user);
+  update(card: Card) {
+    return this.http.put(this.URL + '/' + card.cardId, card);
   }
 
   delete(id: string) {
